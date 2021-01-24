dd88a1c982363001d20118cf1f84509fe3c7ee16
Vignesh Natarajan <vickyemailbox@gmail.com>
2021-01-24 16:08:07 +0100

    Makefile

--- src/json.h
+++ src/json.h
@@ -1,7 +1,41 @@
+/**
+ *
+ * @FILE        	: JSON.H 
+ * @DESCRIPTION 	: LIBRARY TO CREATE SIMPLE JSON OBJECT
+ *
+ * @AUTHOR      	: VIKI (A) VIGNESH NATARAJAN
+ * @CONTACT         : VIKILABS.ORG
+ *
+ */
+
+/*
+    Copyright (c) 2016 Viki( Vignesh Natarajan )
+
+    Contact @ vikilabs.org
+
+    Permission is hereby granted, free of charge, to any person obtaining
+    a copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the Software
+    is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef JSON_H
+#define JSON_H
 /* Library to create simple json object  
  * 
- * This library can be used for free. Feel free to use, modify and distribute
- *
  *                          Author  : Vignesh Natarajan (a) Viki
  *                          Contact : viki@vikilabs.org 
  */
@@ -78,4 +112,4 @@ int8_t json_object_append(char **object, char *key, char *value, json_value_t ty
  *          - JSON_ARRAU
  */
 int8_t json_array_append(char **array, char *value, json_value_t type);
-
+#endif
